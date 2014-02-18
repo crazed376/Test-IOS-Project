30b0af597baa8a56fc0175c0d8713a49708773b3
Rich <crazed@Richs-MacBook-Pro.local>
2014-02-18 22:28:53 +1100

    added test class

---
+++ GIT Test/CRTest.h
@@ -0,0 +1,13 @@
+//
+//  CRTest.h
+//  GIT Test
+//
+//  Created by Rich on 2/18/14.
+//  Copyright (c) 2014 Chrissy and Rich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRTest : NSObject
+
+@end
---
+++ GIT Test/CRTest.m
@@ -0,0 +1,13 @@
+//
+//  CRTest.m
+//  GIT Test
+//
+//  Created by Rich on 2/18/14.
+//  Copyright (c) 2014 Chrissy and Rich. All rights reserved.
+//
+
+#import "CRTest.h"
+
+@implementation CRTest
+
+@end
